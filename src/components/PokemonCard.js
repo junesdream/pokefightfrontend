eaf93bd9529434b9a1d3cb9bf3d5f57ade887c22
June <norisori@gmail.com>
2021-11-05 15:27:10 +0100

    second push

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -19,20 +19,29 @@ const PokemonCard = () => {
       SpDefense: 65,
       Speed: 45,
     },
-  }
+  };
+
+  const playMore = () => {
+     alert("Great Pokemon!");
+  };
 
   return (
-    <div className='pokemoncard_container'>
-      <h3>{pokemon.name.english}</h3>
-      <img src={pokemon.image} alt={pokemon.name.english} />
-      <p>id: {pokemon.id}</p>
-      <p>
-        {pokemon.type.map((t) => (
-          <li>{t}</li>
-        ))}
-      </p>
-    </div>
-  )
+		<div className="pokemoncard_container">
+			<h3>{pokemon.name.english}</h3>
+			<img src={pokemon.image} alt={pokemon.name.english} />
+			<p>id: {pokemon.id}</p>
+			<p>
+				{pokemon.type.map((t) => (
+					<li>{t}</li>
+				))}
+			</p>
+			<div>
+				<form onsubmit="console.log('You clicked submit.'); return false">
+					<button onclick={playMore}> Change the Pokemon!</button>
+				</form>
+			</div>
+		</div>
+  );
 }
 
-export default PokemonCard
+export default PokemonCard;
